24dd87fdde17c08588ef4705740e9c6ff861abf6
Anton CAZALET <acazalet@protonmail.com>
2021-10-30 14:58:37 +0200

    Add get by movie & delete a movie calls

--- mymovie/src/routes/movies.ts
+++ mymovie/src/routes/movies.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from 'mongoose';
 import { Movie } from "../database/schema/movies";
 
 const router = express.Router();
@@ -16,6 +17,74 @@ router.get("/", async (req: express.Request, res: express.Response) => {
     res.status(200).send({
         "success": true,
         "data": result,
+    });
+});
+
+router.get("/:id", async (req: express.Request, res: express.Response) => {
+    const { id } = req.params;
+
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).send({
+            "success": true,
+            "data": "Bad request",
+        });
+        return;
+    }
+
+    const movie = await Movie.findById(id).exec();
+
+    if (!movie) {
+        res.status(404).send({
+            "success": true,
+            "data": "Not found",
+        });
+        return;
+    }
+
+    res.status(200).send({
+        "success": true,
+        "data": {
+            id: movie._id,
+            title: movie.title,
+            synopsis: movie.synopsis,
+            release_date: movie.release_date
+        },
+    });
+});
+
+router.delete("/:id", async (req: express.Request, res: express.Response) => {
+    const { id } = req.params;
+
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).send({
+            "success": true,
+            "data": "Bad request",
+        });
+        return;
+    }
+
+    const movie = await Movie.findById(id).exec();
+
+    if (!movie) {
+        res.status(404).send({
+            "success": true,
+            "data": "Not found",
+        });
+        return;
+    }
+
+    let movie_copy = JSON.parse(JSON.stringify(movie));
+
+    movie.delete();
+
+    res.status(200).send({
+        "success": true,
+        "data": {
+            id: movie_copy._id,
+            title: movie_copy.title,
+            synopsis: movie_copy.synopsis,
+            release_date: movie_copy.release_date
+        },
     });
 });
 
