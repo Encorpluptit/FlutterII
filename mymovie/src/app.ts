da6d4e1aa0484ca34cc43d266a4d3b636282e992
Anton CAZALET <acazalet@protonmail.com>
2021-10-30 22:41:38 +0200

    Add prod & env connection to mongo

--- mymovie/src/app.ts
+++ mymovie/src/app.ts
@@ -7,8 +7,8 @@ import debug from './routes/debug';
 
 const app = express();
 
-const mongo_host = process.env.DB_HOST || 'localhost';
-const mongo_uri = `mongodb://${mongo_host}:27017/MyMovie`;
+const mongo_host = process.env.NODE_ENV === 'production' ? '+srv://dbUser:Jf1HNOIbLraplYzM@mymovie-api.6y4dx.mongodb.net' : '://localhost:27017';
+const mongo_uri = `mongodb${mongo_host}/myMovie?retryWrites=true&w=majority`;
 
 mongoose.connect(mongo_uri, function (err) {
     if (err) {
