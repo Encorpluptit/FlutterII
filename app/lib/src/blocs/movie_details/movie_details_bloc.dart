24947dcaedde97a9bf368e29b54a1f0e6ba10daa
Anton CAZALET <acazalet@protonmail.com>
2021-11-09 19:33:57 +0100

    Remove useless packages

--- app/lib/src/blocs/movie_details/movie_details_bloc.dart
+++ app/lib/src/blocs/movie_details/movie_details_bloc.dart
@@ -1,31 +1,31 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:meta/meta.dart';
+import 'package:movieapp/src/blocs/bloc.dart';
 import 'package:movieapp/src/models/movie.dart';
 import 'package:movieapp/src/resources/repository.dart';
 
 part 'movie_details_event.dart';
 part 'movie_details_state.dart';
 
-class MovieDetailsBloc extends Bloc<MovieDetailsEvent, MovieDetailsState> {
+class MovieDetailsBloc extends Bloc<MovieDetailsState, MovieDetailsEvent> {
   final Repository repository = Repository();
-  MovieDetailsBloc() : super(MovieDetailsLoading());
+  MovieDetailsBloc();
 
   @override
-  Stream<MovieDetailsState> mapEventToState(MovieDetailsEvent event) async* {
+  Future<void> mapEventToState(MovieDetailsEvent event) async {
     if (event is MovieDetailsLoadEvent || event is MovieDetailsRefreshEvent) {
-      yield* _MovieDetailsLoad();
+      setState(await _MovieDetailsLoad());
     }
   }
 
-  Stream<MovieDetailsState> _MovieDetailsLoad() async* {
+  Future<MovieDetailsState> _MovieDetailsLoad() async {
     try {
       //TODO
       final movies = await repository.fetchMovieDetails("state") as dynamic;
       //TODO
-      yield MovieDetailsLoadedSuccess(movies);
+      return MovieDetailsLoadedSuccess(movies);
     } catch (_) {
-      yield MovieDetailsLoadedFailure();
+      return MovieDetailsLoadedFailure();
     }
   }
 }
--- app/lib/src/ui/routes/main_routes.dart
+++ app/lib/src/ui/routes/main_routes.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movieapp/src/blocs/provider.dart';
 import 'package:movieapp/src/blocs/routes/routes_bloc.dart';
 
