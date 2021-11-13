f44ecb02b66a8491acef871cf7faa5ccde9277ba
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-11-13 20:31:56 +0100

    Add style for the Account Screen

--- app/lib/src/ui/widgets/account.dart
+++ app/lib/src/ui/widgets/account.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movieapp/src/blocs/account/account_bloc.dart';
 import 'package:movieapp/src/blocs/provider.dart';
-import 'package:movieapp/src/models/movie.dart';
-
-import 'movie_item.dart';
 
 class AccountView extends StatefulWidget {
   final bool loggedIn;
@@ -18,40 +16,98 @@ class AccountView extends StatefulWidget {
 class _AccountViewState extends State<AccountView> {
   @override
   Widget build(BuildContext context) {
-    if (!widget.loggedIn) {
-      return (Scaffold(
-          body: Center(
-        child: Column(
-          children: [
-            const Text(
-              "Welcome Guest",
-              style: TextStyle(fontSize: 20),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                Provider.getBloc<AccountBloc>()
-                    .dispatch(AccountClickOnLogInEvent());
-              },
-              child: const Text('Login'),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                Provider.getBloc<AccountBloc>()
-                    .dispatch(AccountClickOnRegisterEvent());
-              },
-              child: const Text('Register'),
-            ),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(5)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.grey.shade200,
+              offset: const Offset(2, 4),
+              blurRadius: 5,
+              spreadRadius: 2)
+        ],
+        gradient: const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xffE6E6E6),
+            Color(0xff14279B),
           ],
         ),
-      )));
-    } else {
-      return (Scaffold(
-          body: Center(
-        child: Text(
-          'Welcome ${widget.username}',
-          style: const TextStyle(fontSize: 20),
-        ),
-      )));
-    }
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+                text: 'MyMovie',
+                style: GoogleFonts.portLligatSans(
+                  textStyle: Theme.of(context).textTheme.headline1,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                )),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          InkWell(
+            onTap: () {
+              Provider.getBloc<AccountBloc>()
+                  .dispatch(AccountClickOnLogInEvent());
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.symmetric(vertical: 13),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(5)),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: const Color(0xffE6E6E6).withAlpha(100),
+                        offset: const Offset(2, 4),
+                        blurRadius: 8,
+                        spreadRadius: 2)
+                  ],
+                  color: Colors.white),
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          GestureDetector(
+            onTap: () {
+              Provider.getBloc<AccountBloc>()
+                  .dispatch(AccountClickOnRegisterEvent());
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.symmetric(vertical: 13),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+                border: Border.all(color: Colors.white, width: 2),
+              ),
+              // ignore: prefer_const_constructors
+              child: Text(
+                'Register now',
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
   }
 }
