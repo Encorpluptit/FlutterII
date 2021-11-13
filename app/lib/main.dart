c075be77deae0f7eeba31bc5fcce37ac68c7a071
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-11-13 18:26:39 +0100

    Add account bloc

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movieapp/src/blocs/account/account_bloc.dart';
 import 'package:movieapp/src/blocs/bloc.dart';
 import 'package:movieapp/src/blocs/movie_details/movie_details_bloc.dart';
 import 'package:movieapp/src/blocs/movie_list/movie_list_bloc.dart';
@@ -24,6 +25,7 @@ void main() {
     BlocInit(MovieListBloc(), MovieListLoadEvent()),
     BlocInit(RoutesBloc(), null),
     BlocInit(MovieDetailsBloc(), null),
+    BlocInit(AccountBloc(), null),
   ].toList());
   runApp(const MyApp());
 }
---
+++ app/lib/src/blocs/account/account_bloc.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+import 'package:movieapp/src/blocs/bloc.dart';
+import 'package:movieapp/src/resources/repository.dart';
+
+part 'account_event.dart';
+part 'account_state.dart';
+
+class AccountBloc extends Bloc<AccountState, AccountEvent> {
+  final Repository repository = Repository();
+
+  AccountBloc() : super(AccountGuest());
+
+  @override
+  Future<void> mapEventToState(Object event) async {}
+}
